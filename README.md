718d2f4e5c4e79dc2957ec2a3d8c813234c209da
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2020-10-04 12:24:01 +0200

    Update README.md
    Furthermore, make it easier to keep README and usage description
    consistent.

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ Will update `~/.config/mimeapps.list` to add mime type associations for all
 mime types that can be opened as listed in the given application `.desktop`
 file. Will update `~/.config/mimeapps.list` such that it first contains a new
 `[Default Applications]` section with a sorted list of all existing, newly
-added and overwritten mime type associations after processing the given
-`.desktop` file.
+added, and overwritten mime type associations after processing the given
+`.desktop` file, then the remaining contents of `~/.config/mimeapps.list`.
--- xdg-mime-default-all.py
+++ xdg-mime-default-all.py
@@ -3,14 +3,18 @@
 import argparse
 import os
 import sys
+import textwrap
 
-parser = argparse.ArgumentParser(description=
-        'Will update "~/.config/mimeapps.list" to add mime type associations for '
-        'all mime types that can be opened as listed in the given application .desktop file. '
-        'Will update "~/.config/mimeapps.list" such that it first contains a new '
-        '"[Default Applications]" section with a sorted list of all existing, '
-        'newly added and overwritten mime type associations after processing the '
-        'given .desktop file.')
+description = '''
+Will update `~/.config/mimeapps.list` to add mime type associations for all
+mime types that can be opened as listed in the given application `.desktop`
+file. Will update `~/.config/mimeapps.list` such that it first contains a new
+`[Default Applications]` section with a sorted list of all existing, newly
+added, and overwritten mime type associations after processing the given
+`.desktop` file, then the remaining contents of `~/.config/mimeapps.list`.
+'''
+
+parser = argparse.ArgumentParser(description=description)
 parser.add_argument('desktop_file', metavar='<application .desktop file>', type=str,
         help='Path to the application .desktop file, usually in "/usr/share/applications".')
 
